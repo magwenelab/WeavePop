05f6d1b55c5b1504f74680000b19b08111bab304
Czirion <claudia.zirionmartinez@duke.edu>
2024-02-13 17:51:38 -0500

    update median filter log

--- workflow/scripts/median_filtering.py
+++ workflow/scripts/median_filtering.py
@@ -1,13 +1,21 @@
-from scipy import ndimage
-import numpy as np
-import pandas as pd
+import sys
 
-cov = pd.read_csv(snakemake.input[0], sep = ",", header = 0)
+with open(snakemake.log[0], "w") as f:
+    sys.stderr = sys.stdout = f
+    
+with open("logs/ploidy/test.log", "w") as f:
+    sys.stderr = sys.stdout = f
+    
+    from scipy import ndimage
+    import numpy as np
+    import pandas as pd
 
-cov_array = np.array(cov["Norm_Median"])
+    cov = pd.read_csv(snakemake.input[0], sep = ",", header = 0)
 
-smoothed_array = ndimage.median_filter(cov_array, size=snakemake.params[0])
+    cov_array = np.array(cov["Norm_Median"])
 
-cov['Smooth']=pd.Series(smoothed_array)
+    smoothed_array = ndimage.median_filter(cov_array, size=snakemake.params[0])
 
-cov.to_csv(snakemake.output[0], index=False, header = True, sep=',')
+    cov['Smooth']=pd.Series(smoothed_array)
+
+    cov.to_csv(snakemake.output[0], index=False, header = True, sep=',')
