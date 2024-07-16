34eb5dd13a106a3a772de3d1f0f4263ae9bd5b3b
Czirion <claudia.zirionmartinez@duke.edu>
2024-07-16 14:17:15 -0400

    change locus to gene_id

--- workflow/scripts/build_sequences_db.py
+++ workflow/scripts/build_sequences_db.py
@@ -12,11 +12,11 @@ def fasta_to_df(fasta, sample, seq_type):
     for record in SeqIO.parse(fasta, "fasta"):
         transcript_id = record.id
         seq_description = record.description
-        locus = seq_description.split(" ")[1]
-        locus = locus.replace("gene", "locus")
+        gene_id = seq_description.split(" ")[1]
+        gene_id = gene_id.replace("gene", "gene_id")
         accession = seq_description.split(" ")[2]
         accession = accession.replace("seq_id", "accession")
-        new_description = sample + "|" + transcript_id + " " + locus + " " + accession
+        new_description = sample + "|" + transcript_id + " " + gene_id + " " + accession
         seq = str(record.seq)
         df = pd.DataFrame({"sample":sample, "transcript_id":transcript_id, "seq":seq, "seq_type":seq_type,  "seq_description":new_description}, index=[0])
         if df.isnull().values.any() or (df == '').any().any():
