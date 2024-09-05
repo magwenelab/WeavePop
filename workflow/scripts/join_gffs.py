d66f9cf39e22d6146138ff8c727b9b3d7f92eef2
Czirion <claudia.zirionmartinez@duke.edu>
2024-09-05 18:55:54 -0400

    Add columns to gff table to include in database

--- workflow/scripts/join_gffs.py
+++ workflow/scripts/join_gffs.py
@@ -24,11 +24,21 @@ def join_gff(output, lineage_tsv):
         dfs.append(df)
     print("Concatenate dataframes into one")
     df = pd.concat(dfs)
-    keep_columns = ['seq_id', 'source_tag', 'primary_tag', 'start', 'end', 'score', 'strand', 'frame', 'ID', 'Parent', 'locus', 'Name', 'description', 'old_ID', 'lineage']
+    keep_columns = [
+        'seq_id', 'source_tag', 'primary_tag', 'start', 'end', 'score', 'strand', 
+        'frame', 'ID', 'Parent', 'locus', 'Name', 'description', 'old_ID', 'lineage',
+        'matches_ref_protein','missing_start_codon', 'missing_stop_codon', 'inframe_stop_codon']
     existing_columns = [column for column in keep_columns if column in df.columns]
     df = df[existing_columns]
+    print("Replacing True/False for Yes/No")
+    df = df.replace({True: 'Yes', False: 'No'})
     print("Renaming columns")
-    df = df.rename(columns={'seq_id': 'accession', 'ID': 'feature_id', 'Name': 'gene_name', 'locus': 'gene_id', 'old_ID': 'old_feature_id'})
+    df = df.rename(columns={
+        'seq_id': 'accession',
+        'ID': 'feature_id',
+        'Name': 'gene_name',
+        'locus': 'gene_id',
+        'old_ID': 'old_feature_id'})
     print("Saving")
     df.to_csv(output, sep='\t', index=False)
 
