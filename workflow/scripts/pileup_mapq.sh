ddd3141b1c52487d125a499c5296ac8abe604816
Czirion <claudia.zirionmartinez@duke.edu>
2024-09-07 21:11:58 -0400

    convert mapq.bed to 0-based

--- workflow/scripts/pileup_mapq.sh
+++ workflow/scripts/pileup_mapq.sh
@@ -3,12 +3,12 @@ samtools mpileup --output-extra MAPQ ${snakemake_input[0]} 2> ${snakemake_log[0]
         split($7, values, ",")
         for (i = 1; i <= $4; i++) sum += values[i]
         avg = sum / $4
-        end = $2 + 1
-        print $1,$2,end,avg 
+        pos = $2 - 1 # convert to 0-based
+        print $1,pos,$2,avg 
     } else {
-        end = $2 + 1
-        print $1,$2,end,"NA"
+        pos = $2 - 1 # convert to 0-based
+        print $1,pos,$2,"NA"
     }
     }' > ${snakemake_output[0]} 2>> ${snakemake_log[0]}
 
-gunzip ${snakemake_input[1]}  --stdout 2>> ${snakemake_log[0]} | cut -f1,2,3 2>> ${snakemake_log[0]} | bedtools map -a - -b ${snakemake_output[0]} -c 4 -o mean -null 0 > ${snakemake_output[1]} 2>> ${snakemake_log[0]}
+gunzip ${snakemake_input[1]}  --stdout 2>> ${snakemake_log[0]} | cut -f1,2,3 2>> ${snakemake_log[0]} | bedtools map -a - -b ${snakemake_output[0]} -c 3 -o mean -null 0 > ${snakemake_output[1]} 2>> ${snakemake_log[0]}
