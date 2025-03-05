5d32e97b35103bed66f13f400c2aaa79510507ad
Czirion <claudia.zirionmartinez@duke.edu>
2025-03-05 16:23:42 -0500

    Make truncated version of distribution plot

--- workflow/scripts/depth_distribution_plots.R
+++ workflow/scripts/depth_distribution_plots.R
@@ -12,6 +12,10 @@ sample <- snakemake@wildcards$sample
 depth<- read.table(snakemake@input[[1]], header = TRUE, stringsAsFactors = TRUE, sep = "\t")
 chrom_names <- read.csv(snakemake@input[[2]], header = TRUE, col.names = c("lineage", "accession", "chromosome"))
 
+# sample <- "ERS542301"
+# depth <- read.table("/FastData/czirion/Crypto_Diversity_Pipeline/Crypto_Ashton/results/04.Intermediate_files/01.Samples/depth_quality/ERS542301/depth_distribution.tsv", header = TRUE, stringsAsFactors = TRUE, sep = "\t")
+# chrom_names <- read.csv("/FastData/czirion/Crypto_Diversity_Pipeline/Crypto_Ashton/results/02.Dataset/chromosomes.csv", header = TRUE, col.names = c("lineage", "accession", "chromosome"))
+
 print("Adding 0 to null depth values...")
 depth[is.na(depth)] <- 0
 
@@ -55,6 +59,7 @@ plot_chrom <- ggplot(depth, aes(x=depth))+
         panel.border = element_rect(colour = "lightgray", fill=NA, linewidth = 2))
 
 
+
 print("Calculating global depth distribution...")
 depth_global <- depth %>%
   select(depth, count_good, count_raw)%>%
@@ -69,11 +74,48 @@ plot_global <- ggplot()+
   scale_y_log10(name = "Number of Sites", labels = comma)+
   scale_x_continuous(name = "Depth (X) ", labels = comma, n.breaks = 10)+
   scale_fill_manual(name= "Alignment quality", values= color_quality)+
+  theme_bw()+
+  theme(legend.position="none",
+    axis.title = element_blank(),
+    panel.background = element_rect(fill = "white"),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+  # labs(title = paste("Lineage:",lineage, " Sample:", sample,  sep = " "))
+
+# getting mode
+max_depth <- depth_global %>%
+  filter(count_good_global == max(count_good_global)) %>%
+  pull(depth)
+
+# Truncated plot
+plot_truncated <- ggplot()+
+  geom_col(data = depth_global, aes(x=depth, y = count_raw_global, fill= "All alignments"))+ 
+  geom_col(data = depth_global, aes(x=depth,y = count_good_global, fill= "Good quality alignments"))+ 
+  scale_y_log10(name = "Number of Sites", labels = comma)+
+  scale_x_continuous(name = "Depth (X) ", labels = comma, n.breaks = 10, limits = c(0,max_depth*10))+
+  scale_fill_manual(name= "Alignment quality", values= color_quality)+
   theme(legend.position="none")+
   labs(title = paste("Lineage:",lineage, " Sample:", sample,  sep = " "))+
   theme_bw()
 
+overlay_grob <- ggplotGrob(plot_global)
+
+# Add the overlay plot to the main plot
+combined_plot <- plot_truncated +
+  annotation_custom(grob = overlay_grob,
+      xmin = 0.5 * max_depth * 10,
+      xmax = max_depth * 10, 
+      ymin = 0.5 * max(log10(depth_global$count_good_global)),
+      ymax = max(log10(depth_global$count_good_global)))
+
 print("Saving plots...")
 ggsave(snakemake@output[[1]], plot = plot_chrom, units = "in", height = 4.5, width = 8, dpi = 600)
-ggsave(snakemake@output[[2]], plot = plot_global,  units = "in", height = 4.5, width = 8, dpi = 600)
+# ggsave(snakemake@output[[2]], plot = plot_global,  units = "in", height = 4.5, width = 8, dpi = 600)
+ggsave(snakemake@output[[2]], plot = combined_plot,  units = "in", height = 4.5, width = 8, dpi = 600)
+
+
 print("Done!")
+
+
+
+
